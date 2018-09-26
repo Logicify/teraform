da5d931ed1ab2ce59b886c56516e04ff07d72863
Dmitry Berezovsky <corvis@users.noreply.github.com>
2018-09-27 00:11:03 +0300

    Added private and public IP addresses to the module output

--- modules/selenoid_grid/output.tf
+++ modules/selenoid_grid/output.tf
@@ -7,3 +7,13 @@ output "nodes_asg_id" {
   description = "ID of auto-scaling group of worker nodes"
   value = "${aws_autoscaling_group.selenium-grid-asg.id}"
 }
+
+output "lb_private_ip" {
+  description = "Private IP address of the load balancer instance"
+  value = "${aws_instance.ggr-instance.private_ip}"
+}
+
+output "lb_public_ip" {
+  description = "Public IP address of the load balancer instance"
+  value = "${aws_instance.ggr-instance.public_ip}"
+}
